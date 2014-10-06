95a37d96c2a60d287575674d1f663690e85009c7
Abby Bader <abby.bader@gmail.com>
2014-10-06 16:19:55 -0600

    Repeats the previous refactor for the other two data types.
    Two nice benefits already:
    * No more "how to load" logic in the ProjectDataLoader class
    * no more inline implementations of Runnable
    
    Next up, we tackle the repeated logic in the concrete subtypes of
    ProjectDataLoader.

--- src/main/java/LiveProjectDataReloader.java
+++ src/main/java/LiveProjectDataReloader.java
@@ -8,34 +8,25 @@
 public class LiveProjectDataReloader extends ProjectDataReloader {
     
     private final LoginStatusReloader loginReloader;
+    private final LastUpdateTimeReloader lastUpdateReloader;
+    private final ProjectDetailsReloader projectDetailsReloader;
 
     protected LiveProjectDataReloader(Project project) {
         super(project);
         loginReloader = new LoginStatusReloader(project);
+        lastUpdateReloader = new LastUpdateTimeReloader(project);
+        projectDetailsReloader = new ProjectDetailsReloader(project);
     }
 
     @Override
     protected void reloadProjectData() {
         // load details every other reload attempt
         if (reloadsCounter % 2 == 0) {
-            new Thread(new Runnable() {
-                
-                @Override
-                public void run() {
-                    loadProjectDetails();
-                }
-            }).start();
+            new Thread(projectDetailsReloader).start();
         }
         
         //do this often
-        new Thread(new Runnable() {
-            
-            @Override
-            public void run() {
-                loadLastUpdateTime();
-                
-            }
-        }).start();
+        new Thread(lastUpdateReloader).start();
         
         // don't need this very often..
         // load login statistics every five hundred reload attempts
