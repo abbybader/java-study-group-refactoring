8cf8b95e64ba058746347091d8dd87579d0b1634
Abby Bader <abby.bader@gmail.com>
2014-10-13 13:41:35 -0600

    Rewrites ProjectDataReloader subtypes to work on List<DataReloader>
    Now that we have the "DataReloader" abstraction, we can create and
    iterate over a list of DataReloaders in our scheduleDataLoading methods.
    This might seem like overkill, but it will make sense in the next step.

--- src/main/java/LiveProjectDataReloader.java
+++ src/main/java/LiveProjectDataReloader.java
@@ -1,33 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Reloader for a "live" project, where project data is expected to change
  * frequently.
  * 
  */
 public class LiveProjectDataReloader extends ProjectDataReloader {
+    
+    private List<DataReloader> dataReloaders;
 
     protected LiveProjectDataReloader(Project project) {
         super(project);
+        dataReloaders = new ArrayList<>();
+        dataReloaders.add(new ProjectDetailsLoader(project));
+        dataReloaders.add(new LastUpdateTimeLoader(project));
+        dataReloaders.add(new LoginStatisticsLoader(project));
     }
 
-    /**
-     * A "live" project needs to periodically reload "project details," "last update
-     * time", and "login statistics."  The ScheduledExecutorService "executor" will
-     * periodically (about once every RELOAD_PERIOD) execute the runnables defined
-     * below, which will invoke those loading methods.
-     */
     @Override
     protected void scheduleDataLoading() {
 
-        // Define the tasks
-        DataReloader projectDetailLoader = new ProjectDetailsLoader(project);
-        DataReloader lastUpdateTimeLoader = new LastUpdateTimeLoader(project);
-        DataReloader loginStatisticsLoader = new LoginStatisticsLoader(project);
-        
-        // Now schedule the tasks
-        // executor is inherited from ProjectDataReloader        
-        executor.scheduleAtFixedRate(projectDetailLoader, 0, RELOAD_PERIOD, RELOAD_PERIOD_UNIT);
-        executor.scheduleAtFixedRate(lastUpdateTimeLoader, 0, RELOAD_PERIOD, RELOAD_PERIOD_UNIT);
-        executor.scheduleAtFixedRate(loginStatisticsLoader, 0, RELOAD_PERIOD, RELOAD_PERIOD_UNIT);
+        for (DataReloader reloader : dataReloaders ) { 
+            executor.scheduleAtFixedRate(reloader, 0, RELOAD_PERIOD, RELOAD_PERIOD_UNIT);
+        }
     }
 
 }
--- src/main/java/StaticProjectDataReloader.java
+++ src/main/java/StaticProjectDataReloader.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A reloader for projects that are no longer "live."  Project details and data
  * are not expected to change.
  *
  */
 public class StaticProjectDataReloader extends ProjectDataReloader {
+    
+    protected List<DataReloader> dataReloaders;
 
     protected StaticProjectDataReloader(Project project) {
         super(project);
+        dataReloaders = new ArrayList<>();
+        dataReloaders.add(new LoginStatisticsLoader(project));
     }
 
-    /**
-     * A "static" project's data doesn't change anymore, so we only need to refresh
-     * "login statistics."  The executor will periodically (about once every 
-     * RELOAD_PERIOD) execute the runnable defined below, which will invoke the login-
-     * related loading method.
-     */
+ 
     @Override
     protected void scheduleDataLoading() {
-
-        DataReloader loginStatisticsLoader = new LoginStatisticsLoader(project);
-        
-        executor.scheduleAtFixedRate(loginStatisticsLoader, 0, RELOAD_PERIOD, RELOAD_PERIOD_UNIT);
+        for (DataReloader reloader : dataReloaders) {
+            executor.scheduleAtFixedRate(reloader, 0, RELOAD_PERIOD, RELOAD_PERIOD_UNIT);
+        }
     }
 
 }
